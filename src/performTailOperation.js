c2d8f1a6a7aef15bcfbbc94ce9c2dfea8d1c41c9
Step 25 <bhuvanadeepika017@gmail.com>
2019-12-26 14:56:05 +0530

    modified name of result

--- src/performTailOperation.js
+++ src/performTailOperation.js
@@ -10,18 +10,18 @@ const performTailOperation = function(cmdArgs, fsUtils) {
   const { isFileExist, reader, encoding } = fsUtils;
   const userOptions = filterUserOptions(cmdArgs);
   const parsedOptions = parseUserOptions(userOptions);
-  const result = { lastLines: "", error: "" };
+  const tailResult = { lastLines: "", error: "" };
 
   if (!isFileExist(parsedOptions.filePath)) {
-    const filePath = parsedOptions.filePath,
-      msg = "no such file or directory";
-    result.error = `tail: ${filePath}: ${msg}`;
-    return result;
+    const filePath = parsedOptions.filePath;
+    let msg = "no such file or directory";
+    tailResult.error = `tail: ${filePath}: ${msg}`;
+    return tailResult;
   }
 
   let fileContent = reader(parsedOptions.filePath, encoding);
-  result.lastLines = getLastLines(fileContent, parsedOptions.noOfLines);
-  return result;
+  tailResult.lastLines = getLastLines(fileContent, parsedOptions.noOfLines);
+  return tailResult;
 };
 
 module.exports = { performTailOperation };

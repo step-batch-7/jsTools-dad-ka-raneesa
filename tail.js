7cbd388fbd884b586be0d80a1f0319cae41937db
Step 25 <bhuvanadeepika017@gmail.com>
2020-01-08 19:05:17 +0530

    modified main

--- tail.js
+++ tail.js
@@ -5,16 +5,15 @@ const { stdin, stdout, stderr } = require('process');
 const { tail } = require('./src/performTail');
 const StreamPicker = require('./src/streamPicker');
 
-const printEndResult = function({ error, lastLines }) {
-  stderr.write(error);
-  stdout.write(lastLines);
-};
-
 const createStdinStream = () => stdin;
 
 const main = function(cmdArgs) {
   const streamPicker = new StreamPicker(createReadStream, createStdinStream);
-  tail(cmdArgs, streamPicker, printEndResult);
+  tail(cmdArgs, streamPicker, ({ error, lastLines }) => {
+    stderr.write(error);
+    stdout.write(lastLines);
+  }
+  );
 };
 
 main(process.argv);
